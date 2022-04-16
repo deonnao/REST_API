b8e0203c59fe81616cd45565295afdd81673d8fe
deonnao <82995772+deonnao@users.noreply.github.com>
2022-04-16 17:55:10 -0500

    Add files via upload

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,1 +1,83 @@
+const express = require('express')
+const subscriber = require('../models/subscriber')
+const router = express.Router()
+const Subscriber = require('../models/subscriber')
 
+// Getting all
+router.get('/', async (req, res) => {
+  try {
+    const subscribers = await Subscriber.find()
+    res.json(subscribers)
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+})
+
+// Getting One
+router.get('/:id', getSubscriber, (req, res) => {
+  res.json(res.subscriber)
+})
+
+// Creating one
+router.post('/', async (req, res) => {
+  const subscriber = new Subscriber({
+    name: req.body.name,
+    subscribedToChannel: req.body.subscribedToChannel
+  })
+  try {
+    const newSubscriber = await subscriber.save()
+    res.status(201).json(newSubscriber)
+  } catch (err) {
+    res.status(400).json({ message: err.message })
+  }
+})
+
+// Updating One
+router.patch('/:id', getSubscriber, async (req, res) => {
+  
+  if(req.body.name != null) {
+    res.subscriber.name = req.body.name
+  }
+  if(req.body.subscribedToChannel != null) {
+    res.subscriber.subscribedToChannel = req.body.subscribedToChannel
+  }
+
+  try {
+    const updatedSubscriber = await res.subscriber.save()
+    res.json(updatedSubscriber)
+  } 
+  catch (err) {
+    res.status(400).json({ message: err.message})
+  }
+
+})
+
+// Deleting One
+router.delete('/:id', getSubscriber, async (req, res) => {
+  try {
+    await res.subscriber.remove()
+    res.json({ message: 'Deleted subscriber' })
+  } 
+  catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+})
+
+async function getSubscriber(req, res, next) {
+  let subscriber;
+  try {
+    subscriber = await Subscriber.findById(req.params.id)
+    if(subscriber == null) {
+      return res.status(404).json({ message: 'Cannot find subscriber' }) //404 -- could not find something
+    }
+  } 
+  catch (err) {
+    return res.status(500).json({ message: err.message }) //500 -- something wrong with my server
+  }
+
+  res.subscriber = subscriber
+  next()
+}
+
+
+module.exports = router
